f7578e19b67316bcb022eebc518280e039934911
Aleksey <aleksey19988@gmail.com>
2022-10-06 15:11:45 +0300

    Added migrations for tables

---
+++ migrations/m221006_114928_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%orders}}`.
+ */
+class m221006_114928_create_orders_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%orders}}', [
+            'id' => $this->primaryKey(),
+            'order_content' => $this->string()->notNull()->comment('Содержание заказа'),
+            'created_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP')->comment('Дата и время создания заказа'),
+            'closed_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP')->comment('Дата и время закрытия заказа'),
+            'fk_order_status' => $this->integer()->defaultValue(1),
+            'total' => $this->integer()->comment('Сумма заказа'),
+        ]);
+
+        $this->addForeignKey('fk_d_order_statuses_to_orders_fk_order_status', '{{%orders}}', 'fk_order_status', '{{%d_order_statuses}}', 'id');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_d_order_statuses_to_orders_fk_order_status', '{{%orders}}');
+        $this->dropTable('{{%orders}}');
+    }
+}
