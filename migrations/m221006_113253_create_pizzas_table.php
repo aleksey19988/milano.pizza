b030be5dfcd1ca9a28b689446857f6304358b89a
Aleksey <aleksey19988@gmail.com>
2022-10-19 12:08:14 +0300

    Удалил ненужные миграции

--- migrations/m221006_113253_create_pizzas_table.php
+++ migrations/m221006_113253_create_pizzas_table.php
@@ -3,23 +3,22 @@
 use yii\db\Migration;
 
 /**
- * Handles the creation of table `{{%d_pizzas}}`.
+ * Handles the creation of table `{{%pizzas}}`.
  */
-class m221006_113253_create_d_pizzas_table extends Migration
+class m221006_113253_create_pizzas_table extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-        $this->createTable('{{%d_pizzas}}', [
+        $this->createTable('{{%pizzas}}', [
             'id' => $this->primaryKey(),
             'title' => $this->string()->notNull()->comment('Название пиццы'),
             'weight' => $this->integer()->comment('Вес пиццы'),
-            'fk_diameter' => $this->integer()->comment('Диаметр пиццы'),
-            'ingredients' => $this->integer()->comment('Ингредиенты'),
             'price' => $this->integer()->comment('Стоимость пиццы'),
             'piece_price' => $this->integer()->comment('Стоимость кусочка пиццы'),
+            'image_path' => $this->string()->comment('Расположение изображения'),
             'is_active' => $this->boolean()->notNull()->defaultValue(true)->comment('Активен'),
         ]);
 
@@ -31,7 +30,7 @@ class m221006_113253_create_d_pizzas_table extends Migration
      */
     public function safeDown()
     {
-        $this->dropForeignKey('fk_diameters_id_to_d_pizzas_diameter_id', '{{%d_pizzas}}');
+        $this->dropForeignKey('fk_diameters_id_to_d_pizzas_fk_diameter', '{{%d_pizzas}}');
         $this->dropTable('{{%d_pizzas}}');
     }
 }
